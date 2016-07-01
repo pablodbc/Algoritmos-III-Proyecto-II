7c29e23602a9d10886feb16a1f0cc88fce3393e0
pablodbc <pablod_bc@hotmail.com>
2016-07-01 14:58:55 -0400

    SolverRPP lectura y construccion de los grafos listo

--- SolverRPP.java
+++ SolverRPP.java
@@ -4,8 +4,8 @@ public class SolverRPP{
 	private static Integer V;
 	private static Integer aristasRequeridas;
 	private static Integer aristasNoRequeridas;	
-	private static EdgeWeightedDigraph G;
-	private static EdgeWeightedDigraph Gr;
+	private static EdgeWeightedGraph G;
+	private static EdgeWeightedGraph Gr;
 
 	public static void readFile(In in){
 		in.readLine(); //Nombre
@@ -15,17 +15,27 @@ public class SolverRPP{
 		aristasNoRequeridas = Integer.valueOf(in.readLine().replaceAll("[^0-9]+", "")); // Aristas no requeridas
 		in.readLine(); //Lista Aristas Req
 		String[] lineaActual;
-		Integer[] Aux = new Integer[4];
+		Integer[] Aux = new Integer[2];
 
+		// Se leen las aristas requeridas y se agregan al grafo G y al grafo Gr
 		for(int i = 0; i<aristasRequeridas; i++){
 			lineaActual = in.readLine().replaceAll("[^0-9]+", " ").trim().split(" ");
-			for(int j = 0; j<4; j++) Aux[j] = Integer.valueOf(lineaActual[j]);
+			for(int j = 0; j<2; j++) Aux[j] = Integer.valueOf(lineaActual[j]);
+			Integer u = Aux[0] - 1,v = Aux[1] - 1;
+			Double w = Double.valueOf(lineaActual[2]);
+			Edge e = new Edge(u,v,w);
+			G.addEdge(e);
+			Gr.addEdge(e);
 		}
 
-		
-		for(int i = 0; i<aristasRequeridas; i++){
+		// Se leen las aristas no requeridas y se agregan al grafo G
+		for(int i = 0; i<aristasNoRequeridas; i++){
 			lineaActual = in.readLine().replaceAll("[^0-9]+", " ").trim().split(" ");
-			for(int j = 0; j<4; j++) Aux[j] = Integer.valueOf(lineaActual[j]);
+			for(int j = 0; j<2; j++) Aux[j] = Integer.valueOf(lineaActual[j]);
+			Integer u = Aux[0] - 1,v = Aux[1] - 1;
+			Double w = Double.valueOf(lineaActual[2]);
+			Edge e = new Edge(u,v,w);
+			G.addEdge(e);
 		}
 
 	}
