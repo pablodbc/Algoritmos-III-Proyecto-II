a7c5ea177b6f060e6a9000ee3bae502d8cda6f81
pablodbc <pablod_bc@hotmail.com>
2016-07-01 01:30:45 -0400

    Avance leyendo archivo + archivo de prueba

--- SolverRPP.java
+++ SolverRPP.java
@@ -1,5 +1,24 @@
 import java.util.*;
 
-public class SolverRpp{
-	
+public class SolverRPP{
+	private static Integer V;
+	private static Integer req;
+	private static Integer no_req;	
+	private static EdgeWeightedDigraph G;
+
+	public static void readFile(In in){
+		in.readLine(); //Nombre
+		in.readLine(); //Comentario
+		V = Integer.valueOf(in.readLine().replaceAll("[^0-9]+", "")); //Numero de vertices
+		req = Integer.valueOf(in.readLine().replaceAll("[^0-9]+", "")); //Aristas requeridas
+		no_req = Integer.valueOf(in.readLine().replaceAll("[^0-9]+", "")); // Aristas no requeridas
+		System.out.printf("%d %d %d\n",V,req,no_req);
+
+
+	}
+	public static void main(String[] args){
+		In in = new In(args[0]);
+		readFile(in);
+	}
+
 }
